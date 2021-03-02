2b271c31ad9128bc05fd1d132f1fc48a2d6e3ca7
uladislausamets <vladislavsamec@gmail.com>
2021-03-02 15:07:17 +0300

    UIRS-20 Rename accession and retrieval configurations

--- src/const.js
+++ src/const.js
@@ -2,8 +2,7 @@ export const STORAGES_LIST_ROUTE = '/settings/remote-storage';
 
 export const SECTIONS_STORAGE = {
   INFORMATION: 'information',
-  ACCESSION: 'accession',
-  RETRIEVAL: 'retrieval',
+  SYNCHRONIZATION: 'synchronization',
 };
 
 export const TIME_UNITS = [
