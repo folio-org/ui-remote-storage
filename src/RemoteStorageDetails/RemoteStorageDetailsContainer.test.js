77964314614d72bdf562ba0e23b8849d203bb0e1
Alex Mozolev <aleksey_mozolev@epam.com>
2021-03-03 15:36:03 +0300

    refactor RemoteStorageDetailsContainer test not to use act(render())

--- src/RemoteStorageDetails/RemoteStorageDetailsContainer.test.js
+++ src/RemoteStorageDetails/RemoteStorageDetailsContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, act, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 
 import RemoteStorageDetailsContainer from './RemoteStorageDetailsContainer';
@@ -23,7 +23,7 @@ const renderRemoteStorageDetailsContainer = ({
 describe('RemoteStorageDetailsContainer', () => {
   let mutator;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     RemoteStorageDetails.mockClear();
 
     mutator = {
@@ -35,44 +35,44 @@ describe('RemoteStorageDetailsContainer', () => {
         GET: jest.fn(() => Promise.resolve({ mappings: [] })),
       },
     };
-
-    await act(async () => {
-      renderRemoteStorageDetailsContainer({ mutator });
-    });
   });
 
   it('should fetch configurations', async () => {
-    expect(mutator.configurations.GET).toHaveBeenCalled();
+    renderRemoteStorageDetailsContainer({ mutator });
+
+    await waitFor(() => expect(mutator.configurations.GET).toHaveBeenCalled());
   });
 
   it('should render Remote Storage Details', async () => {
-    expect(screen.getByText('RemoteStorageDetails')).toBeDefined();
+    renderRemoteStorageDetailsContainer({ mutator });
+
+    await waitFor(() => expect(screen.getByText('RemoteStorageDetails')).toBeVisible());
   });
 
   describe('Remove RS', () => {
     it('should fetch mappings', async () => {
-      await waitFor(() => {
-        RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
-      });
+      renderRemoteStorageDetailsContainer({ mutator });
 
-      expect(mutator.mappings.GET).toHaveBeenCalled();
+      RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
+      await waitFor(() => expect(mutator.mappings.GET).toHaveBeenCalled());
     });
 
     it('should open remove confirmation modal', async () => {
-      await waitFor(() => {
-        RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
-      });
+      renderRemoteStorageDetailsContainer({ mutator });
 
-      expect(screen.getByText('ui-remote-storage.removingModal.message')).toBeDefined();
+      RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
+      await waitFor(() => expect(screen.getByText('ui-remote-storage.removingModal.message')).toBeDefined());
     });
 
     it('should call DELETE configuration', async () => {
-      await waitFor(async () => {
-        RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
-        user.click(screen.getByText('ui-remote-storage.confirmationModal.save'));
-      });
+      renderRemoteStorageDetailsContainer({ mutator });
 
-      expect(mutator.configurations.DELETE).toHaveBeenCalled();
+      RemoteStorageDetails.mock.calls[0][0].onRemovestorage();
+      const confirmationModalButton = await screen.findByText('ui-remote-storage.confirmationModal.save');
+
+      user.click(confirmationModalButton);
+
+      await waitFor(() => expect(mutator.configurations.DELETE).toHaveBeenCalled());
     });
   });
 });
