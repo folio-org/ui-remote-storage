f52067ba8db64a2ad8b97cbf5241271ba933c0ee
Alex Mozolev <aleksey_mozolev@epam.com>
2021-03-04 17:40:21 +0300

    refactor RemoteStorageForm tests

--- src/RemoteStorageForm/RemoteStorageForm.test.js
+++ src/RemoteStorageForm/RemoteStorageForm.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
 
 import '@folio/stripes-acq-components/test/jest/__mock__';
 
 import RemoteStorageForm from './RemoteStorageForm';
 
-jest.mock('@folio/stripes-components/lib/Layer', () => {
-  // eslint-disable-next-line react/prop-types
-  return ({ children }) => (
-    <>{children}</>
-  );
-});
+jest.mock(
+  '@folio/stripes-components/lib/Layer',
+  () => props => props.children,
+);
 
 const renderRemoteStorageForm = ({
   initialValues,
-  providers = [{ label: 'DEMATIC_SD' }],
+  providers = [],
   onSubmit = jest.fn(),
   onClose = jest.fn(),
   pristine = true,
@@ -35,25 +33,28 @@ const renderRemoteStorageForm = ({
 ));
 
 describe('RemoteStorageForm', () => {
-  it('should open create form if no initialValues', async () => {
+  it('should open create form if no initialValues', () => {
     renderRemoteStorageForm();
 
-    await waitFor(() => expect(screen.getByText('ui-remote-storage.createForm.title')).toBeDefined());
+    expect(screen.getByText('ui-remote-storage.createForm.title')).toBeVisible();
   });
 
-  it('should open create form if initialValues exist', async () => {
+  it('should open create form if initialValues exist', () => {
     renderRemoteStorageForm({ initialValues: {} });
 
-    await waitFor(() => expect(screen.getByText('ui-remote-storage.editForm.title')).toBeDefined());
+    expect(screen.getByText('ui-remote-storage.editForm.title')).toBeVisible();
   });
 
-  it('should show Status URL if Dematic SD choosed', async () => {
-    renderRemoteStorageForm({ initialValues: {} });
-
-    await waitFor(() => {
-      user.selectOptions(screen.getByLabelText('ui-remote-storage.details.providerName'), 'DEMATIC_SD');
+  it('should show Status URL if Dematic SD chosen', () => {
+    renderRemoteStorageForm({
+      initialValues: {},
+      providers: [{ label: 'DEMATIC_SD' }, { label: 'DEMATIC_EMS' }],
     });
 
-    expect(screen.getByLabelText('ui-remote-storage.details.statusUrl')).toBeDefined();
+    user.selectOptions(screen.getByLabelText('ui-remote-storage.details.providerName'), 'DEMATIC_SD');
+    expect(screen.queryByLabelText('ui-remote-storage.details.statusUrl')).toBeVisible();
+
+    user.selectOptions(screen.getByLabelText('ui-remote-storage.details.providerName'), 'DEMATIC_EMS');
+    expect(screen.queryByLabelText('ui-remote-storage.details.statusUrl')).not.toBeInTheDocument();
   });
 });
