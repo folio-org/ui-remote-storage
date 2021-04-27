edb64202815e7df4026aed43f08c2002fcd5747a
Alex Mozolev <aleksey_mozolev@epam.com>
2021-04-27 16:00:19 +0300

    UIRS-32 Remove CaiaSoft accession workflow preference

--- src/Details/CaiaSoft/Sections.js
+++ src/Details/CaiaSoft/Sections.js
@@ -8,29 +8,19 @@ import {
   Row,
 } from '@folio/stripes/components';
 
-import { AccessionWorkflow } from './AccessionWorkflow';
 import { ReturningWorkflow } from './ReturningWorkflow';
 
 export const Sections = ({ isNonInteractive }) => {
   const intl = useIntl();
 
   return (
-    <>
-      <Accordion label={intl.formatMessage({ id: 'ui-remote-storage.accession-workflow.title' })}>
-        <Row>
-          <Col xs={6}>
-            <AccessionWorkflow isNonInteractive={isNonInteractive} />
-          </Col>
-        </Row>
-      </Accordion>
-      <Accordion label={intl.formatMessage({ id: 'ui-remote-storage.returning-workflow.title' })}>
-        <Row>
-          <Col xs={6}>
-            <ReturningWorkflow isNonInteractive={isNonInteractive} />
-          </Col>
-        </Row>
-      </Accordion>
-    </>
+    <Accordion label={intl.formatMessage({ id: 'ui-remote-storage.returning-workflow.title' })}>
+      <Row>
+        <Col xs={6}>
+          <ReturningWorkflow isNonInteractive={isNonInteractive} />
+        </Col>
+      </Row>
+    </Accordion>
   );
 };
 
