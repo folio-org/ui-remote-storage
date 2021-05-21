4ca890a2e8e9bcda907fa1943695096ecda9cfd2
Alex Mozolev <aleksey_mozolev@epam.com>
2021-05-21 09:25:08 +0300

    add Delete scenario component

--- src/Configurations/Delete/UI.js
+++ src/Configurations/Delete/UI.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 
@@ -8,31 +8,27 @@ import {
   ModalFooter,
 } from '@folio/stripes/components';
 
-import { Configurations, Mappings } from '../../../API';
-import { LoadingCentered } from '../../../components';
+import { LoadingCentered } from '../../components';
 
 
 const Button = props => <OriginalButton marginBottom0 {...props} />;
 
 
-export const DeleteConfirmation = ({ configurationId, onConfirm, onCancel, open, ...rest }) => {
+export const UI = ({
+  configuration,
+  isLoading,
+  isError,
+  isDeletePossible,
+  onConfirm,
+  onCancel,
+  ...rest
+}) => {
   const intl = useIntl();
-  const [isDeletePossible, setIsDeletePossible] = useState(false);
-
-  const configurationQuery = Configurations.useSingleQuery({ id: configurationId, enabled: open });
-  const mappingsQuery = Mappings.useListQuery({
-    enabled: open,
-    onSuccess: ({ mappings }) => {
-      if (!mappings.some(item => item.configurationId === configurationId)) {
-        setIsDeletePossible(true);
-      }
-    },
-  });
 
   const confirmation = {
     label: intl.formatMessage(
       { id: 'ui-remote-storage.removingModal.title' },
-      { name: configurationQuery.configuration?.name },
+      { name: configuration?.name ?? '' },
     ),
     message: intl.formatMessage({ id: 'ui-remote-storage.removingModal.message' }),
     footer: (
@@ -52,10 +48,7 @@ export const DeleteConfirmation = ({ configurationId, onConfirm, onCancel, open,
     message: intl.formatMessage({ id: 'ui-remote-storage.cannotDeleteModal.message' }),
     footer: (
       <ModalFooter>
-        <Button
-          buttonStyle="primary"
-          onClick={onCancel}
-        >
+        <Button buttonStyle="primary" onClick={onCancel}>
           {intl.formatMessage({ id: 'stripes-core.label.okay' })}
         </Button>
       </ModalFooter>
@@ -64,7 +57,7 @@ export const DeleteConfirmation = ({ configurationId, onConfirm, onCancel, open,
 
   const loading = {
     ...confirmation,
-    message: <LoadingCentered />,
+    message: <LoadingCentered size="large" />,
     footer: <ModalFooter />,
   };
 
@@ -73,32 +66,29 @@ export const DeleteConfirmation = ({ configurationId, onConfirm, onCancel, open,
     message: intl.formatMessage({ id: 'ui-remote-storage.error' }),
   };
 
-  const isLoading = mappingsQuery.isFetching || configurationQuery.isLoading;
-  const isError = mappingsQuery.isError || configurationQuery.isError;
-
   const content = (isLoading && loading) || (isError && error) || (isDeletePossible && confirmation) || impossible;
 
   return (
-    <>
-      <Modal
-        size="small"
-        open={open}
-        label={content.label}
-        footer={content.footer}
-        {...rest}
-      >
-        {content.message}
-      </Modal>
-
-    </>
+    <Modal
+      size="small"
+      label={content.label}
+      footer={content.footer}
+      {...rest}
+    >
+      {content.message}
+    </Modal>
   );
 };
 
-DeleteConfirmation.propTypes = {
+UI.propTypes = {
   ...Modal.propTypes,
-  configurationId: PropTypes.string.isRequired,
+  configuration: PropTypes.object,
+  isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
+  isDeletePossible: PropTypes.bool,
   onConfirm: PropTypes.func.isRequired,
   onCancel: PropTypes.func.isRequired,
+
   label: PropTypes.node, // to make in not required
   children: PropTypes.node, // to make in not required
 };
