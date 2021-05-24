d9621f3ab43da0d254514630cf366729c27862c8
Alex Mozolev <aleksey_mozolev@epam.com>
2021-05-24 14:20:20 +0300

    Revert "fix smell"
    This reverts commit e25ca80f

--- src/API/useOkapiQuery.js
+++ src/API/useOkapiQuery.js
@@ -2,10 +2,10 @@ import { useQuery } from 'react-query';
 
 import { useOkapiKy } from '@folio/stripes/core';
 
-export const useOkapiQuery = ({ path, searchParams, ...options }) => {
+export const useOkapiQuery = ({ path, searchParams, ...rest }) => {
   const ky = useOkapiKy();
   const queryKey = [path, searchParams];
   const queryFn = () => ky(path, { searchParams }).json();
 
-  return useQuery(queryKey, queryFn, options);
+  return useQuery({ queryKey, queryFn, ...rest });
 };
