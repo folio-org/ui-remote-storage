e25ca80f192b042da2be80d673d5d3ef98b91dcb
Alex Mozolev <aleksey_mozolev@epam.com>
2021-05-24 13:12:17 +0300

    fix smell

--- src/API/useOkapiQuery.js
+++ src/API/useOkapiQuery.js
@@ -2,10 +2,10 @@ import { useQuery } from 'react-query';
 
 import { useOkapiKy } from '@folio/stripes/core';
 
-export const useOkapiQuery = ({ path, searchParams, ...rest }) => {
+export const useOkapiQuery = ({ path, searchParams, ...options }) => {
   const ky = useOkapiKy();
   const queryKey = [path, searchParams];
   const queryFn = () => ky(path, { searchParams }).json();
 
-  return useQuery({ queryKey, queryFn, ...rest });
+  return useQuery(queryKey, queryFn, options);
 };
