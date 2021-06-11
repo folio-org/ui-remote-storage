c67f3318f101c98376572daf7a3398780ad3590c
Alex Mozolev <aleksey_mozolev@epam.com>
2021-06-11 09:08:14 +0300

    add configurations options test

--- src/AccessionTables/AccessionTables.test.js
+++ src/AccessionTables/AccessionTables.test.js
@@ -28,15 +28,33 @@ beforeEach(() => {
       configurations: [
         {
           id: '1',
-          name: 'CAIA1',
+          name: 'CaiaSoft Configuration 1',
           providerName: 'CAIA_SOFT',
           accessionTimeUnit: 'minutes',
           metadata: { 'createdDate': '2021-05-28T10:08:08.216+00:00' },
         },
         {
           id: '2',
-          name: 'CAIA2',
+          name: 'CaiaSoft Configuration 2',
           providerName: 'CAIA_SOFT',
+          url: 'http://rs2.dematic.com',
+          accessionDelay: 2,
+          accessionTimeUnit: 'minutes',
+          metadata: { 'createdDate': '2021-05-28T01:53:57.036+00:00' },
+        },
+        {
+          id: '3',
+          name: 'Dematic SD Configuration',
+          providerName: 'DEMATIC_SD',
+          url: 'http://rs2.dematic.com',
+          accessionDelay: 2,
+          accessionTimeUnit: 'minutes',
+          metadata: { 'createdDate': '2021-05-28T01:53:57.036+00:00' },
+        },
+        {
+          id: '4',
+          name: 'Dematic EMS Configuration',
+          providerName: 'DEMATIC_EMS',
           url: 'http://rs2.dematic.com',
           accessionDelay: 2,
           accessionTimeUnit: 'minutes',
@@ -61,8 +79,8 @@ const renderAccessionTables = () => {
   );
 };
 
-describe('Selection', () => {
-  it('displays the selection', async () => {
+describe('Configuration selection', () => {
+  it('has only CaiaSoft options', async () => {
     renderAccessionTables();
 
     await screen.findByRole('button');
@@ -71,7 +89,27 @@ describe('Selection', () => {
 
     user.click(selection);
 
-    expect(screen.getByRole('option', { name: 'CAIA1' })).toBeVisible();
-    expect(screen.getByRole('option', { name: 'CAIA2' })).toBeVisible();
+    expect(screen.getByRole('option', { name: 'CaiaSoft Configuration 1' })).toBeVisible();
+    expect(screen.getByRole('option', { name: 'CaiaSoft Configuration 2' })).toBeVisible();
+    expect(screen.queryByRole('option', { name: 'Dematic SD Configuration' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('option', { name: 'Dematic EMS Configuration' })).not.toBeInTheDocument();
+  });
+
+  it('filters the options', async () => {
+    renderAccessionTables();
+
+    await screen.findByRole('button');
+
+    const selection = screen.getByRole('button', { expanded: false });
+
+    user.click(selection);
+
+    expect(screen.getByRole('option', { name: /1/ })).toBeVisible();
+    expect(screen.getByRole('option', { name: /2/ })).toBeVisible();
+
+    user.type(screen.getByRole('combobox', { name: /selection/i }), '1');
+
+    expect(screen.getByRole('option', { name: /1/ })).toBeVisible();
+    expect(screen.queryByRole('option', { name: /2/ })).not.toBeInTheDocument();
   });
 });
