f0f304a31b3d6e8e8fe2b7e0c348a7b82a4eb116
vashjs <86330150+vashjs@users.noreply.github.com>
2021-07-07 20:13:05 +0300

    UIRS-59 Unify configurations sorting order over the module (#106)

--- src/data/Configurations/queries.js
+++ src/data/Configurations/queries.js
@@ -1,16 +1,14 @@
-import { orderBy } from 'lodash';
-
 import { Configurations } from '../../API';
 
 
 export const useListQuery = options => {
   const query = Configurations.useListQuery(options);
 
-  const configurations = orderBy(query.configurations, ['name']);
+  const configurations = [...query.configurations].sort((a, b) => a.name.localeCompare(b.name));
 
   return {
-    configurations,
     ...query,
+    configurations,
   };
 };
 
