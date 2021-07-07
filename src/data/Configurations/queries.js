f4fc3ab72e68d3693e73dffdc5bccb2bb8d9c2da
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-07-07 18:00:40 +0300

    UIRS-59 Unify configurations sorting order over the module

--- src/data/Configurations/queries.js
+++ src/data/Configurations/queries.js
@@ -1,12 +1,11 @@
-import { orderBy } from 'lodash';
-
 import { Configurations } from '../../API';
+import { compare } from '../Locations';
 
 
 export const useListQuery = options => {
   const query = Configurations.useListQuery(options);
 
-  const configurations = orderBy(query.configurations, ['name']);
+  const configurations = query.configurations.sort(compare);
 
   return {
     configurations,
