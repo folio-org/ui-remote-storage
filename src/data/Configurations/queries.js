21a5506161994939c51aec6480c27ffb21084253
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-07-07 18:18:41 +0300

    UIRS-59 add localeCompare by name

--- src/data/Configurations/queries.js
+++ src/data/Configurations/queries.js
@@ -1,11 +1,10 @@
 import { Configurations } from '../../API';
-import { compare } from '../Locations';
 
 
 export const useListQuery = options => {
   const query = Configurations.useListQuery(options);
 
-  const configurations = query.configurations.sort(compare);
+  const configurations = query.configurations.sort((a, b) => a.name.localeCompare(b.name));
 
   return {
     configurations,
