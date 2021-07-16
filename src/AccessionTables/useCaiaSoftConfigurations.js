059aae30f28cd508ccca82f9dc4c87b95c72b01a
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-07-16 11:41:07 +0300

    UIRS-57 remove unused options

--- src/AccessionTables/useCaiaSoftConfigurations.js
+++ src/AccessionTables/useCaiaSoftConfigurations.js
@@ -1,8 +1,8 @@
 import { Configurations } from '../data';
 import { CAIASOFT } from '../const';
 
-export const useCaiaSoftConfigurations = (options) => {
-  const query = Configurations.useListQuery(options);
+export const useCaiaSoftConfigurations = () => {
+  const query = Configurations.useListQuery();
 
   const configurations = query.configurations.filter(({ providerName }) => providerName === CAIASOFT);
 
