99f9e8df9605811068e5f0943b18a37cce7b5ac4
uladislausamets <uladislau_samets@epam.com>
2021-08-02 11:59:52 +0300

    f

--- src/data/Configurations/queries.js
+++ src/data/Configurations/queries.js
@@ -1,7 +1,9 @@
 import { Configurations } from '../../API';
 
+
 export const useListQuery = options => {
   const query = Configurations.useListQuery(options);
+
   const configurations = [...query.configurations].sort((a, b) => a.name.localeCompare(b.name));
 
   return {
